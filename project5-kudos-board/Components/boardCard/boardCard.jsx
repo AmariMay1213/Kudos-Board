650f7f801b8516753b2cf158757a5bd508dd30eb
knyuga <kennanyuga@gmail.com>
2025-07-03 19:37:38 -0700

    title on card page (#33)
    Co-authored-by: jessicaobi <149744440+jessicaobi@users.noreply.github.com>

--- project5-kudos-board/Components/boardCard/boardCard.jsx
+++ project5-kudos-board/Components/boardCard/boardCard.jsx
@@ -26,7 +26,8 @@ function BoardCard({board}) {
           <div className="buttons">
 
             {/* Need to add a click that brings the user to the card route whenever they click view board */}
-            <Link to = {`/boards/${board.board_Id}`}>
+            <Link to = {`/boards/${board.board_Id}`}
+              state={{ title: board.title }}>
             <i className="material-icons">
       
               View Board
